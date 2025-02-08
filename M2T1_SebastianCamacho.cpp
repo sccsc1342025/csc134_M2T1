7507b2dd6a9ad2fb23beaf06d3d38b0a9a513aa7
sccsc1342025 <sebastiancam711@gmail.com>
2025-02-08 20:19:00 +0000

    M2T1 done

--- M2T1_SebastianCamacho.cpp
+++ M2T1_SebastianCamacho.cpp
@@ -9,6 +9,18 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello" << endl;
+    string name = "Sebastian Camacho";
+    int num_apples = 10;
+    double price_each = 0.25;
+
+    cout << "Welcome to the " << name << " apple farm!" << endl;
+    cout << "We have " << num_apples << " in stock" << endl;
+    cout << "Each apple costs $" << price_each << endl;
+
+    double total_price = num_apples * price_each;
+
+    cout << "The total price for all " << num_apples;
+    cout << " is: $" << total_price << endl;
+    cout << "Thank you for coming by!" << endl;
     return 0;
 }
